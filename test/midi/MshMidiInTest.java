daf0533672ddcbdb6d811148a4a81f98042a119f
Stephane Letz <letz@users.sourceforge.net>
2000-07-01 16:48:29 +0000

    Import

---
+++ test/midi/MshMidiInTest.java
@@ -0,0 +1,52 @@
+/*
+ *	MshMidiInTest.java
+ */
+
+
+import	javax.sound.midi.Sequence;
+import	javax.sound.midi.Track;
+import	javax.sound.midi.Transmitter;
+import	javax.sound.midi.Receiver;
+import	javax.sound.midi.InvalidMidiDataException;
+import	javax.sound.midi.MidiUnavailableException;
+import	javax.sound.midi.MidiDevice;
+import	javax.sound.midi.MidiSystem;
+
+import	org.tritonus.midi.device.midishare.MshMidiDevice;
+
+import	org.tritonus.TDebug;
+
+
+
+public class MshMidiInTest
+{
+	public static void main(String[] args)
+	{
+		org.tritonus.TDebug.TraceInit = false;
+		MidiDevice.Info[]	infos = MidiSystem.getMidiDeviceInfo();
+		TDebug.out("after MidiSystem.getMidiDeviceInfo()");
+	
+		MshMidiDevice	device = new MshMidiDevice();
+		Receiver	r = new DumpReceiver(System.out);
+		
+		try
+		{
+			device.open();
+			Transmitter	t = device.getTransmitter();
+			t.setReceiver(r);
+		}
+		catch (MidiUnavailableException e)
+		{
+		}
+		while (true)
+		{
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch (InterruptedException e)
+			{
+			}
+		}
+	}
+}
---
+++ test/midi/MshMidiOutTest.java
@@ -0,0 +1,60 @@
+/*
+ *	MshMidiOutTest.java
+ */
+
+import	javax.sound.midi.Transmitter;
+import	javax.sound.midi.Receiver;
+import	javax.sound.midi.ShortMessage;
+import	javax.sound.midi.InvalidMidiDataException;
+import	javax.sound.midi.MidiUnavailableException;
+
+import	org.tritonus.midi.device.midishare.MshMidiDevice;
+
+
+
+public class MshMidiOutTest
+{
+	public static void main(String[] args)
+	{
+		
+		MshMidiDevice	device = new MshMidiDevice();	
+		Receiver	r = null;
+		try
+		{
+			device.open();
+			r = device.getReceiver();
+
+		}
+		catch (MidiUnavailableException e)
+		{
+		}
+		System.out.println("Receiver: " + r);
+		
+		for (int i = 0; i<50; i++){
+			try
+			{
+				ShortMessage	m0 = new ShortMessage();
+				m0.setMessage(0x90, 0, 61, 80);
+				//r.send(m0, -1);  // for current date
+				r.send(m0, device.getMicroSecondPosition() + 1005500);
+				try
+				{
+					Thread.sleep(1000);
+				}
+				catch (InterruptedException e)
+				{
+				}
+				ShortMessage	m1 = new ShortMessage();
+				m1.setMessage(0x80, 0, 61, 0);
+				//r.send(m1, -1);  // for current date
+				r.send(m1, device.getMicroSecondPosition() + 1005500);
+			}
+			catch (InvalidMidiDataException e)
+			{
+			}
+		}
+
+		r.close();
+		device.close();
+	}
+}
