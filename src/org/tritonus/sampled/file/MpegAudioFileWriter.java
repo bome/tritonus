e9748a65e6c20fcc1a7682be97ebdea08028b4de
Florian Bomers <github@bome.com>
2000-08-11 23:16:20 +0000

    added for use with GSM. No mp3 encoding possible yet.

---
+++ src/org/tritonus/sampled/file/MpegAudioFileWriter.java
@@ -0,0 +1,102 @@
+/*
+ *	MpegAudioFileWriter.java
+ */
+
+/*
+ *  Copyright (c) 2000 by Florian Bomers <florian@bome.com>
+ *
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as published
+ *   by the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+
+package	org.tritonus.sampled.file;
+
+
+import	java.io.File;
+import	java.io.InputStream;
+import	java.io.IOException;
+import	java.io.OutputStream;
+import	java.io.DataOutputStream;
+import	java.util.Arrays;
+
+import	javax.sound.sampled.AudioFileFormat;
+import	javax.sound.sampled.AudioFormat;
+import	javax.sound.sampled.AudioInputStream;
+import	javax.sound.sampled.AudioSystem;
+
+import	org.tritonus.TDebug;
+
+/**
+ * Class for writing mpeg files
+ *
+ * @author Florian Bomers
+ */
+public class MpegAudioFileWriter extends TAudioFileWriter {
+
+	private static final AudioFileFormat.Type[]	FILE_TYPES =
+	{
+	    MpegFileFormatType.MPEG
+	};
+	
+	private static final AudioFormat[]	AUDIO_FORMATS =
+	{
+		new AudioFormat(MpegEncoding.MPEG1L3, 44100, 16, 1, 
+				AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, false),
+		new AudioFormat(MpegEncoding.MPEG1L3, 44100, 16, 1, 
+				AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, true),
+		new AudioFormat(MpegEncoding.MPEG1L3, 44100, 16, 2, 
+				AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, false),
+		new AudioFormat(MpegEncoding.MPEG1L3, 44100, 16, 2, 
+				AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, true),
+	};
+	
+	public MpegAudioFileWriter()
+	{
+		super(Arrays.asList(FILE_TYPES),
+		      Arrays.asList(AUDIO_FORMATS));
+	}
+
+
+	protected AudioOutputStream getAudioOutputStream(
+		AudioFormat audioFormat,
+		long lLengthInBytes,
+		AudioFileFormat.Type fileType,
+		File file)
+		throws	IOException
+	{
+		TDataOutputStream	dataOutputStream = new SeekableTDOS(file);
+		return new HeaderLessAudioOutputStream(audioFormat,
+					       lLengthInBytes,
+					       dataOutputStream);
+	}
+
+	protected AudioOutputStream getAudioOutputStream(
+		AudioFormat audioFormat,
+		long lLengthInBytes,
+		AudioFileFormat.Type fileType,
+		OutputStream outputStream)
+		throws	IOException
+	{
+		TDataOutputStream	dataOutputStream = new NonSeekableTDOS(outputStream);
+		return new HeaderLessAudioOutputStream(audioFormat,
+					       lLengthInBytes,
+					       dataOutputStream);
+	}
+	
+}
+
+/*** MpegAudioFileWriter.java ***/
