932bd09fcc08a255102263abf530f3c6861a4ab2
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2000-10-20 07:34:42 +0000

    adapted to changes in Saint constructor (no more output parameters)

--- src/lib/saint/org_tritonus_lowlevel_saint_Saint.cpp
+++ src/lib/saint/org_tritonus_lowlevel_saint_Saint.cpp
@@ -8,7 +8,7 @@
 #include	"org_tritonus_lowlevel_saint_Saint.h"
 #include	"jstreambuf.hpp"
 
-static int	DEBUG = 1;
+// static int	DEBUG = 1;
 
 
 static void
@@ -80,7 +80,8 @@ JNIEXPORT jint JNICALL
 Java_org_tritonus_lowlevel_saint_Saint_getChannelCount
 (JNIEnv *env, jobject obj)
 {
-	return -1;
+	Saint*	saint = getNativeHandle(env, obj);
+	return saint->getChannelCount();
 }
 
 
@@ -94,7 +95,8 @@ JNIEXPORT jint JNICALL
 Java_org_tritonus_lowlevel_saint_Saint_getSamplingRate
 (JNIEnv *env, jobject obj)
 {
-	return -1;
+	Saint*	saint = getNativeHandle(env, obj);
+	return saint->getSamplingRate();
 }
 
 
@@ -102,43 +104,43 @@ Java_org_tritonus_lowlevel_saint_Saint_getSamplingRate
 /*
  * Class:     org_tritonus_lowlevel_saint_Saint
  * Method:    init
- * Signature: (Ljava/io/InputStream;Ljava/io/InputStream;Ljava/io/OutputStream;I)V
+ * Signature: (Ljava/io/InputStream;)V
  */
+JNIEXPORT void JNICALL
+Java_org_tritonus_lowlevel_saint_Saint_init__Ljava_io_InputStream_2
+(JNIEnv *env, jobject obj, jobject bitStream)
+{
+}
+
+
+
+/*
+ * Class:     org_tritonus_lowlevel_saint_Saint
+ * Method:    init
+ * Signature: (Ljava/io/InputStream;Ljava/io/InputStream;)V
+ */
+JNIEXPORT void JNICALL Java_org_tritonus_lowlevel_saint_Saint_init__Ljava_io_InputStream_2Ljava_io_InputStream_2
+(JNIEnv *env, jobject obj, jobject orchestraStream, jobject scoreStream)
+{
+	jstreambuf*	orchestraStreamBuf = new jstreambuf(env, orchestraStream);
+	istream*	orchestraIStream = new istream(orchestraStreamBuf);
+	jstreambuf*	scoreStreamBuf = new jstreambuf(env, scoreStream);
+	istream*	scoreIStream = new istream(scoreStreamBuf);
+	Saint*	saint = new Saint(orchestraIStream, scoreIStream);
+	setNativeHandle(env, obj, saint);
+}
+
+/*
 JNIEXPORT void JNICALL
 Java_org_tritonus_lowlevel_saint_Saint_init__Ljava_io_InputStream_2Ljava_io_InputStream_2Ljava_io_OutputStream_2I
 (JNIEnv *env, jobject obj, jobject orchestraStream, jobject scoreStream, jobject outputStream, jint nOutputFormat)
 {
 	jstreambuf*	orchestraStreamBuf = new jstreambuf(env, orchestraStream);
 	istream*	orchestraIStream = new istream(orchestraStreamBuf);
-	jstreambuf*	scoreStreamBuf = new jstreambuf(env, scoreStream);
-	istream*	scoreIStream = new istream(scoreStreamBuf);
-	jstreambuf*	outputStreamBuf = new jstreambuf(env, outputStream);
-	ostream*	outputOStream = new ostream(outputStreamBuf);
-	Out*		pOut = new Out(outputOStream, nOutputFormat);
-	Saint*	saint = new Saint(orchestraIStream, scoreIStream, pOut);
+	Saint*	saint = new Saint(orchestraIStream, scoreIStream, nOutputFormat);
 	setNativeHandle(env, obj, saint);
 }
-
-
-
-/*
- * Class:     org_tritonus_lowlevel_saint_Saint
- * Method:    init
- * Signature: (Ljava/io/InputStream;Ljava/io/OutputStream;I)V
- */
-JNIEXPORT void JNICALL
-Java_org_tritonus_lowlevel_saint_Saint_init__Ljava_io_InputStream_2Ljava_io_OutputStream_2I
-(JNIEnv *env, jobject obj, jobject bitStream, jobject outputStream, jint nOutputFormat)
-{
-	jstreambuf*	bitStreamBuf = new jstreambuf(env, bitStream);
-	istream*	bitIStream = new istream(bitStreamBuf);
-	jstreambuf*	outputStreamBuf = new jstreambuf(env, outputStream);
-	ostream*	outputOStream = new ostream(outputStreamBuf);
-	Out*		pOut = new Out(outputOStream, nOutputFormat);
-	Saint*	saint = new Saint(bitIStream, pOut);
-	setNativeHandle(env, obj, saint);
-}
-
+*/
 
 
 /*
@@ -154,6 +156,24 @@ Java_org_tritonus_lowlevel_saint_Saint_run
 	saint->run();
 }
 
+
+
+/*
+ * Class:     org_tritonus_lowlevel_saint_Saint
+ * Method:    setOutput
+ * Signature: (Ljava/io/OutputStream;I)V
+ */
+JNIEXPORT void JNICALL
+Java_org_tritonus_lowlevel_saint_Saint_setOutput
+(JNIEnv *env, jobject obj, jobject outputStream, jint nOutputFormat)
+{
+	cerr << "in (native) setOutput()" << endl;
+	jstreambuf*	outputStreamBuf = new jstreambuf(env, outputStream);
+	ostream*	outputOStream = new ostream(outputStreamBuf);
+	Out*	pOut = new Out(outputOStream, nOutputFormat);
+	Saint*	saint = getNativeHandle(env, obj);
+	saint->setOutput(pOut);
+}
 
 
 /*** org_tritonus_lowlevel_saint_Saint.cpp ***/
