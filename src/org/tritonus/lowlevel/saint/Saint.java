d6bed4828bce57bb5a71dc1a80924faffff006af
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2000-10-20 07:21:22 +0000

    removed old comments; added format constants; removed output stream and format argument from constructors and init(); introduced setOutput() instead

--- src/org/tritonus/lowlevel/saint/Saint.java
+++ src/org/tritonus/lowlevel/saint/Saint.java
@@ -2,25 +2,6 @@
  *	Saint.java
  */
 
-/*
-#include	<iostream>
-#include	<fstream>
-#include	<string>
-#include	<cstdio>
-#include	"Saint.hpp"
-
-using	std::string;
-
-// HACK! should be part of a header file
-extern "C" int error_count;
-
-extern "C" void init_scanner(char *name);
-extern "C" void get_token(void);
-extern "C" void orchestra(void);
-extern "C" void pre_scan(void);
-extern "C" void free_orchestra(void);
-*/
-
 package	org.tritonus.lowlevel.saint;
 
 
@@ -37,43 +18,60 @@ public class Saint
 	}
 
 
+	/* copied from /usr/include/linux/asound.h */
+	public static final int		SND_PCM_SFMT_S8 = 0;
+	public static final int		SND_PCM_SFMT_U8	= 1;
+	public static final int		SND_PCM_SFMT_S16_LE = 2;
+	public static final int		SND_PCM_SFMT_S16_BE = 3;
+	public static final int		SND_PCM_SFMT_U16_LE = 4;
+	public static final int		SND_PCM_SFMT_U16_BE = 5;
+	public static final int		SND_PCM_SFMT_S24_LE = 6;	/* low three bytes */
+	public static final int		SND_PCM_SFMT_S24_BE = 7;	/* low three bytes */
+	public static final int		SND_PCM_SFMT_U24_LE = 8;	/* low three bytes */
+	public static final int		SND_PCM_SFMT_U24_BE = 9;	/* low three bytes */
+	public static final int		SND_PCM_SFMT_S32_LE = 10;
+	public static final int		SND_PCM_SFMT_S32_BE = 11;
+	public static final int		SND_PCM_SFMT_U32_LE = 12;
+	public static final int		SND_PCM_SFMT_U32_BE = 13;
+	public static final int		SND_PCM_SFMT_FLOAT_LE = 14;	/* 4-byte float, IEEE-754 32-bit */
+	public static final int		SND_PCM_SFMT_FLOAT_BE = 15;	/* 4-byte float, IEEE-754 32-bit */
+	public static final int		SND_PCM_SFMT_FLOAT64_LE = 16;	/* 8-byte float, IEEE-754 64-bit */
+	public static final int		SND_PCM_SFMT_FLOAT64_BE = 17;	/* 8-byte float, IEEE-754 64-bit */
+	public static final int		SND_PCM_SFMT_IEC958_SUBFRAME_LE = 18;	/* IEC-958 subframe, Little Endian */
+	public static final int		SND_PCM_SFMT_IEC958_SUBFRAME_BE = 19;	/* IEC-958 subframe, Big Endian */
+	public static final int		SND_PCM_SFMT_MU_LAW = 20;
+	public static final int		SND_PCM_SFMT_A_LAW = 21;
+	public static final int		SND_PCM_SFMT_IMA_ADPCM = 22;
+	public static final int		SND_PCM_SFMT_MPEG = 23;
+	public static final int		SND_PCM_SFMT_GSM = 24;
+	public static final int		SND_PCM_SFMT_SPECIAL = 31;
+
+
 	private long	m_lNativeHandle;
 
 
 
-	public Saint(InputStream bitStream,
-		     OutputStream outputStream,
-		     int nOutputFormat)
+	public Saint(InputStream bitStream)
 	{
-		init(bitStream,
-		     outputStream,
-		     nOutputFormat);
+		init(bitStream);
 	}
 
 
 
 	public Saint(InputStream orchestraStream,
-		     InputStream scoreStream,
-		     OutputStream outputStream,
-		     int nOutputFormat)
+		     InputStream scoreStream)
 	{
 		init(orchestraStream,
-		     scoreStream,
-		     outputStream,
-		     nOutputFormat);
+		     scoreStream);
 	}
 
 
 	private native void init(
-		InputStream bitStream,
-		OutputStream outputStream,
-		int nOutputFormat);
+		InputStream bitStream);
 
 	private native void init(
 		InputStream orchestraStream,
-		InputStream scoreStream,
-		OutputStream outputStream,
-		int nOutputFormat);
+		InputStream scoreStream);
 
 
 	// TODO: finalizer??
@@ -99,6 +97,9 @@ Saint::~Saint()
 
 	public native int getChannelCount();
 
+	public native void setOutput(
+		OutputStream outputStream,
+		int nOutputFormat);
 
 	public native void run();
 }
