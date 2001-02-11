9e6ae42ad2c80e1d8bd9695458496a4bc61a91cc
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2001-02-11 16:40:20 +0000

    new: check constants from ASequencer.java againt those of asequencer/soundlib.h

---
+++ src/lib/alsa/constants_check.h
@@ -0,0 +1,416 @@
+
+
+#if SND_SEQ_EVENT_SYSTEM != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SYSTEM
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_RESULT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_RESULT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_NOTE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_NOTE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_NOTEON != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_NOTEON
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_NOTEOFF != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_NOTEOFF
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_KEYPRESS != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_KEYPRESS
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_CONTROLLER != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_CONTROLLER
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_PGMCHANGE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_PGMCHANGE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_CHANPRESS != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_CHANPRESS
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_PITCHBEND != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_PITCHBEND
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_CONTROL14 !=org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_CONTROL14
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_NONREGPARAM != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_NONREGPARAM
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_REGPARAM != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_REGPARAM
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SONGPOS != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SONGPOS
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SONGSEL != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SONGSEL
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_QFRAME != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_QFRAME
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_TIMESIGN != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_TIMESIGN
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_KEYSIGN != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_KEYSIGN
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_START != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_START
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_CONTINUE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_CONTINUE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_STOP != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_STOP
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SETPOS_TICK != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SETPOS_TICK
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SETPOS_TIME != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SETPOS_TIME
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_TEMPO != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_TEMPO
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_CLOCK != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_CLOCK
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_TICK != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_TICK
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_TUNE_REQUEST != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_TUNE_REQUEST
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_RESET != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_RESET
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SENSING != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SENSING
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_ECHO != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_ECHO
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_OSS != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_OSS
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_CLIENT_START != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_CLIENT_START
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_CLIENT_EXIT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_CLIENT_EXIT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_CLIENT_CHANGE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_CLIENT_CHANGE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_PORT_START != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_PORT_START
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_PORT_EXIT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_PORT_EXIT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_PORT_CHANGE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_PORT_CHANGE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_PORT_SUBSCRIBED != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_PORT_SUBSCRIBED
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_PORT_USED != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_PORT_USED
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_PORT_UNSUBSCRIBED != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_PORT_UNSUBSCRIBED
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_PORT_UNUSED != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_PORT_UNUSED
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SAMPLE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SAMPLE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SAMPLE_CLUSTER != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SAMPLE_CLUSTER
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SAMPLE_START != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SAMPLE_START
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SAMPLE_STOP != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SAMPLE_STOP
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SAMPLE_FREQ != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SAMPLE_FREQ
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SAMPLE_VOLUME != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SAMPLE_VOLUME
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SAMPLE_LOOP != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SAMPLE_LOOP
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SAMPLE_POSITION != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SAMPLE_POSITION
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SAMPLE_PRIVATE1 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SAMPLE_PRIVATE1
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR0 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR0
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR1 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR1
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR2 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR2
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR3 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR3
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR4 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR4
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR5 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR5
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR6 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR6
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR7 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR7
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR8 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR8
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR9 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR9
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_BEGIN != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_BEGIN
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_END != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_END
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_INFO != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_INFO
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_INFO_RESULT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_INFO_RESULT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_FINFO != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_FINFO
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_FINFO_RESULT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_FINFO_RESULT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_RESET != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_RESET
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_STATUS != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_STATUS
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_STATUS_RESULT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_STATUS_RESULT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_PUT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_PUT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_GET != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_GET
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_GET_RESULT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_GET_RESULT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_FREE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_FREE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_LIST != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_LIST
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_LIST_RESULT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_LIST_RESULT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_CLUSTER != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_CLUSTER
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_CLUSTER_GET != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_CLUSTER_GET
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_CLUSTER_RESULT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_CLUSTER_RESULT
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_INSTR_CHANGE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_INSTR_CHANGE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_SYSEX != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_SYSEX
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_BOUNCE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_BOUNCE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VAR0 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VAR0
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VAR1 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VAR1
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VAR2 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VAR2
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VAR3 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VAR3
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VAR4 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VAR4
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_IPCSHM != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_IPCSHM
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VARIPC0 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VARIPC0
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VARIPC1 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VARIPC1
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VARIPC2 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VARIPC2
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VARIPC3 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VARIPC3
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_USR_VARIPC4 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_USR_VARIPC4
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_NONE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_NONE
+#error "constants don't match"
+#endif
+#if SND_SEQ_ADDRESS_UNKNOWN != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_ADDRESS_UNKNOWN
+#error "constants don't match"
+#endif
+#if SND_SEQ_ADDRESS_SUBSCRIBERS != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_ADDRESS_SUBSCRIBERS
+#error "constants don't match"
+#endif
+#if SND_SEQ_ADDRESS_BROADCAST != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_ADDRESS_BROADCAST
+#error "constants don't match"
+#endif
+#if SND_SEQ_QUEUE_DIRECT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_QUEUE_DIRECT
+#error "constants don't match"
+#endif
+#if SND_SEQ_TIME_STAMP_TICK != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_TIME_STAMP_TICK
+#error "constants don't match"
+#endif
+#if SND_SEQ_TIME_STAMP_REAL != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_TIME_STAMP_REAL
+#error "constants don't match"
+#endif
+#if SND_SEQ_TIME_STAMP_MASK != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_TIME_STAMP_MASK
+#error "constants don't match"
+#endif
+#if SND_SEQ_TIME_MODE_ABS != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_TIME_MODE_ABS
+#error "constants don't match"
+#endif
+#if SND_SEQ_TIME_MODE_REL != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_TIME_MODE_REL
+#error "constants don't match"
+#endif
+#if SND_SEQ_TIME_MODE_MASK != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_TIME_MODE_MASK
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_LENGTH_FIXED != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_LENGTH_FIXED
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_LENGTH_VARIABLE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_LENGTH_VARIABLE
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_LENGTH_VARUSR != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_LENGTH_VARUSR
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_LENGTH_VARIPC != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_LENGTH_VARIPC
+#error "constants don't match"
+#endif
+#if SND_SEQ_EVENT_LENGTH_MASK != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_EVENT_LENGTH_MASK
+#error "constants don't match"
+#endif
+#if SND_SEQ_PRIORITY_NORMAL != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PRIORITY_NORMAL
+#error "constants don't match"
+#endif
+#if SND_SEQ_PRIORITY_HIGH != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PRIORITY_HIGH
+#error "constants don't match"
+#endif
+#if SND_SEQ_PRIORITY_MASK != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PRIORITY_MASK
+#error "constants don't match"
+#endif
+#if SND_SEQ_CLIENT_SYSTEM != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_CLIENT_SYSTEM
+#error "constants don't match"
+#endif
+#if SND_SEQ_CLIENT_DUMMY != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_CLIENT_DUMMY
+#error "constants don't match"
+#endif
+#if SND_SEQ_CLIENT_OSS != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_CLIENT_OSS
+#error "constants don't match"
+#endif
+#if SND_SEQ_FILTER_BROADCAST != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_FILTER_BROADCAST
+#error "constants don't match"
+#endif
+#if SND_SEQ_FILTER_MULTICAST != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_FILTER_MULTICAST
+#error "constants don't match"
+#endif
+#if SND_SEQ_FILTER_BOUNCE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_FILTER_BOUNCE
+#error "constants don't match"
+#endif
+#if SND_SEQ_FILTER_USE_EVENT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_FILTER_USE_EVENT
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_SYSTEM_TIMER != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_SYSTEM_TIMER
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_SYSTEM_ANNOUNCE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_SYSTEM_ANNOUNCE
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_CAP_READ != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_CAP_READ
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_CAP_WRITE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_CAP_WRITE
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_CAP_SYNC_READ != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_CAP_SYNC_READ
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_CAP_SYNC_WRITE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_CAP_SYNC_WRITE
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_CAP_DUPLEX != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_CAP_DUPLEX
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_CAP_SUBS_READ != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_CAP_SUBS_READ
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_CAP_SUBS_WRITE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_CAP_SUBS_WRITE
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_CAP_NO_EXPORT != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_CAP_NO_EXPORT
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_SPECIFIC != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_SPECIFIC
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_MIDI_GENERIC != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_MIDI_GENERIC
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_MIDI_GM != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_MIDI_GM
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_MIDI_GS != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_MIDI_GS
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_MIDI_XG != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_MIDI_XG
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_MIDI_MT32 != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_MIDI_MT32
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_SYNTH != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_SYNTH
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_DIRECT_SAMPLE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_DIRECT_SAMPLE
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_SAMPLE != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_SAMPLE
+#error "constants don't match"
+#endif
+#if SND_SEQ_PORT_TYPE_APPLICATION != org_tritonus_lowlevel_alsa_ASequencer0_SND_SEQ_PORT_TYPE_APPLICATION
+#error "constants don't match"
+#endif
