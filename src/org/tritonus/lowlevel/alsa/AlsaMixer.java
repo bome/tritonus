097927e3ee47f9ceb84e5ea98d05943d702f2f84
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2001-07-02 18:43:52 +0000

    reorganized native library loading and tracing; rewritten for ALSA 0.9.0

--- src/org/tritonus/lowlevel/alsa/AlsaMixer.java
+++ src/org/tritonus/lowlevel/alsa/AlsaMixer.java
@@ -3,7 +3,7 @@
  */
 
 /*
- *  Copyright (c) 2000 by Matthias Pfisterer <Matthias.Pfisterer@gmx.de>
+ *  Copyright (c) 2001 by Matthias Pfisterer <Matthias.Pfisterer@gmx.de>
  *
  *
  *   This program is free software; you can redistribute it and/or modify
@@ -41,30 +41,50 @@ public class AlsaMixer
 
 	static
 	{
-		if (TDebug.TraceAlsaNative)
+		Alsa.loadNativeLibrary();
+		if (TDebug.TraceAlsaMixerNative)
 		{
-			System.out.println("AlsaMixer.<clinit>(): loading native library tritonusalsa");
+			setTrace(true);
 		}
-		System.loadLibrary("tritonusalsa");
-		setTrace(TDebug.TraceAlsaMixerNative);
 	}
 
 
 
-	public AlsaMixer(String strName)
+	public AlsaMixer(String strMixerName)
 		throws	Exception
 	{
-		if (open(strName) < 0)
+		if (open(0) < 0)
 		{
+			throw new Exception();
+		}
+		if (attach(strMixerName) < 0)
+		{
+			close();
+			throw new Exception();
+		}
+		if (register() < 0)
+		{
+			close();
+			throw new Exception();
+		}
+		if (load() < 0)
+		{
+			close();
 			throw new Exception();
 		}
 	}
 
 
-	private native int open(String strMixerName);
+	private native int open(int nMode);
+	private native int attach(String strCardName);
+
+	// incomplete
+	private native int register();
+	private native int load();
 	public native int close();
 
 
+	public native AlsaMixerElement findElement(String strName, int nIndex);
 
 	public List getControls()
 	{
@@ -88,9 +108,11 @@ public class AlsaMixer
 		System.out.println("num of controls: " + nControlsCount);
 		for (int i = 0; i < nControlsCount; i++)
 		{
+/*
 			AlsaMixerControl	control = new AlsaMixerControl(this, anIndices[i], astrNames[i]);
 			control.read();
 			controlsList.add(control);
+*/
 		}
 		return controlsList;
 	}
@@ -109,37 +131,6 @@ public class AlsaMixer
 	public native int readControlList(int[] anIndices, String[] astrNames);
 
 
-	/**
-	 *	anValues[0]	capabilities mask
-	 *	anValues[1]	active channels mask
-	 *	anValues[2]	muted channels mask
-	 *	anValues[3]	capture channels mask
-	 *	anValues[4]	minimum value
-	 *	anValues[5]	maximum value
-	 *	anValues[6]	front left value
-	 *	anValues[7]	front right value
-	 *	anValues[8]	front center value
-	 *	anValues[9]	rear left value
-	 *	anValues[10]	rear right value
-	 *	anValues[11]	woofer value
-	 *
-	 */
-	public native int readControl(int nIndex, String strName,
-				      int[] anValues);
-
-
-	/**
-	 *	anValues[0]	muted channels mask
-	 *	anValues[1]	capture channels mask
-	 *	anValues[2]	front left value
-	 *	anValues[3]	front right value
-	 *	anValues[4]	front center value
-	 *	anValues[5]	rear left value
-	 *	anValues[6]	rear right value
-	 *	anValues[7]	woofer value
-	 */
-	public native int writeControl(int nIndex, String strName,
-				      int[] anValues);
 
 	private static native void setTrace(boolean bTrace);
 }
