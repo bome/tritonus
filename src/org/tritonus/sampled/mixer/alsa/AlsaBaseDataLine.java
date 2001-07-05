788c53a78f2bda27162e6de9ee3033bf91f16882
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2001-07-05 22:58:50 +0000

    corrected formatting; added setting of access mode in HW params

--- src/org/tritonus/sampled/mixer/alsa/AlsaBaseDataLine.java
+++ src/org/tritonus/sampled/mixer/alsa/AlsaBaseDataLine.java
@@ -110,7 +110,7 @@ public abstract class AlsaBaseDataLine
 		The return value is used by this class to decide if it has
 		to deal with a source (PLAYBACK) or target (CAPTURE)
 		data line.
-	 */
+	*/
 	protected abstract int getAlsaStreamType();
 
 
@@ -144,7 +144,7 @@ public abstract class AlsaBaseDataLine
 		if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.openImpl(): input format: " + format); }
 
 		// hack, only true for pmac
-		boolean			bHWBigEndian = true;
+		boolean			bHWBigEndian = false;
 
 		AudioFormat.Encoding	encoding  = format.getEncoding();
 		boolean			bBigEndian = format.isBigEndian();
@@ -184,124 +184,130 @@ public abstract class AlsaBaseDataLine
 			m_alsaPcm = new AlsaPcm(
 				getAlsaMixer().getPcmName(),
 				getAlsaStreamType(),
-				0 /* no special mode */);
+				0);	// no special mode
 		}
-catch (Exception e)
-{
-	if (TDebug.TraceAllExceptions) { TDebug.out(e); }
-	throw new LineUnavailableException();
-}
-int	nReturn;
-HWParams	hwParams = new HWParams();
-nReturn = m_alsaPcm.getAnyHWParams(hwParams);
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): getAnyHWParams(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-nReturn = m_alsaPcm.setHWParamsFormat(hwParams, nAlsaOutFormat);
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsFormat(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-nReturn = m_alsaPcm.setHWParamsChannels(hwParams, format.getChannels());
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsChannels(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-nReturn = m_alsaPcm.setHWParamsRateNear(hwParams, (int) format.getSampleRate());
+		catch (Exception e)
+		{
+			if (TDebug.TraceAllExceptions) { TDebug.out(e); }
+			throw new LineUnavailableException();
+		}
+		int	nReturn;
+		HWParams	hwParams = new HWParams();
+		nReturn = m_alsaPcm.getAnyHWParams(hwParams);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): getAnyHWParams(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setHWParamsAccess(hwParams, AlsaPcm.SND_PCM_ACCESS_RW_INTERLEAVED);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsFormat(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setHWParamsFormat(hwParams, nAlsaOutFormat);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsFormat(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setHWParamsChannels(hwParams, format.getChannels());
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsChannels(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setHWParamsRateNear(hwParams, (int) format.getSampleRate());
 // int nRate = nReturn;
-if (nReturn < 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsRateNear(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-nReturn = m_alsaPcm.setHWParamsBufferTimeNear(hwParams, 500000);
-int	nBufferTime = nReturn;
-if (nReturn < 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsBufferTimeNear(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-nReturn = m_alsaPcm.setHWParamsPeriodTimeNear(hwParams, nBufferTime / 4);
+		if (nReturn < 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsRateNear(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setHWParamsBufferTimeNear(hwParams, 500000);
+		int	nBufferTime = nReturn;
+		if (nReturn < 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsBufferTimeNear(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setHWParamsPeriodTimeNear(hwParams, nBufferTime / 4);
 // int nPeriodTime = nReturn;
-if (nReturn < 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsPeriodTimeNear(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-nReturn = m_alsaPcm.setHWParams(hwParams);
-if (nReturn < 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setHWParams(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-int	nChunkSize = hwParams.getPeriodSize(null);
-int	nBufferSize = hwParams.getBufferSize();
-if (nChunkSize == nBufferSize)
-{
-	throw new LineUnavailableException("period size is equal to buffer size");
-}
+		if (nReturn < 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setHWParamsPeriodTimeNear(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setHWParams(hwParams);
+		if (nReturn < 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setHWParams(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		int	nChunkSize = hwParams.getPeriodSize(null);
+		int	nBufferSize = hwParams.getBufferSize();
+		if (nChunkSize == nBufferSize)
+		{
+			throw new LineUnavailableException("period size is equal to buffer size");
+		}
 
-SWParams	swParams = new SWParams();
-nReturn = m_alsaPcm.getSWParams(swParams);
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): getSWParams(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-nReturn = m_alsaPcm.setSWParamsSleepMin(swParams, 0);
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsSleepMin(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-nReturn = m_alsaPcm.setSWParamsXrunMode(swParams, AlsaPcm.SND_PCM_XRUN_NONE);
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsXrunMode(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-nReturn = m_alsaPcm.setSWParamsAvailMin(swParams, nChunkSize);
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsAvailMin(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-long	lStartThreshold = (long) ((double) format.getFrameRate() * 1 / 1000000);
-nReturn = m_alsaPcm.setSWParamsStartThreshold(swParams, (int) lStartThreshold);
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsStartThreshold(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-long	lStopThreshold = (long) (nBufferSize + (double) format.getFrameRate() * 0 / 1000000);
-nReturn = m_alsaPcm.setSWParamsStopThreshold(swParams, (int) lStopThreshold);
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsStopThreshold(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
+		SWParams	swParams = new SWParams();
+		nReturn = m_alsaPcm.getSWParams(swParams);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): getSWParams(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setSWParamsSleepMin(swParams, 0);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsSleepMin(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setSWParamsXrunMode(swParams, AlsaPcm.SND_PCM_XRUN_NONE);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsXrunMode(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		nReturn = m_alsaPcm.setSWParamsAvailMin(swParams, nChunkSize);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsAvailMin(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		long	lStartThreshold = (long) ((double) format.getFrameRate() * 1 / 1000000);
+		nReturn = m_alsaPcm.setSWParamsStartThreshold(swParams, (int) lStartThreshold);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsStartThreshold(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		long	lStopThreshold = (long) (nBufferSize + (double) format.getFrameRate() * 0 / 1000000);
+		nReturn = m_alsaPcm.setSWParamsStopThreshold(swParams, (int) lStopThreshold);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setSWParamsStopThreshold(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
 // omitted: xfer_align
-nReturn = m_alsaPcm.setSWParams(swParams);
-if (nReturn != 0)
-{
-	TDebug.out("AlsaBaseDataLine.openImpl(): setSWParams(): " + Alsa.getStringError(nReturn));
-	throw new LineUnavailableException(Alsa.getStringError(nReturn));
-}
-if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.openImpl(): end"); }
-}
+		nReturn = m_alsaPcm.setSWParams(swParams);
+		if (nReturn != 0)
+		{
+			TDebug.out("AlsaBaseDataLine.openImpl(): setSWParams(): " + Alsa.getStringError(nReturn));
+			throw new LineUnavailableException(Alsa.getStringError(nReturn));
+		}
+		if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.openImpl(): end"); }
+	}
 
 
 
-protected void closeImpl()
-{
-if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.closeImpl(): begin"); }
-m_alsaPcm.close();
-if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.closeImpl(): end"); }
-}
+	protected void closeImpl()
+	{
+		if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.closeImpl(): begin"); }
+		m_alsaPcm.close();
+		if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.closeImpl(): end"); }
+	}
 
 
 
@@ -315,33 +321,33 @@ if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.closeImpl(): end"
 */
 
 
-protected void stopImpl()
-{
-	if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.stopImpl(): called"); }
-	int	nReturn = 0;
-	// int	nReturn = m_alsaPcm.flushChannel(AlsaPcm.SND_PCM_CHANNEL_PLAYBACK);
-	if (nReturn != 0)
+	protected void stopImpl()
 	{
-		TDebug.out("flushChannel: " + Alsa.getStringError(nReturn));
+		if (TDebug.TraceSourceDataLine) { TDebug.out("AlsaBaseDataLine.stopImpl(): called"); }
+		int	nReturn = 0;
+		// int	nReturn = m_alsaPcm.flushChannel(AlsaPcm.SND_PCM_CHANNEL_PLAYBACK);
+		if (nReturn != 0)
+		{
+			TDebug.out("flushChannel: " + Alsa.getStringError(nReturn));
+		}
+		// setStarted(false);
 	}
-	// setStarted(false);
-}
 
 
 
-public int available()
-{
-	// TODO:
-	return -1;
-}
-
+	public int available()
+	{
+		// TODO:
+		return -1;
+	}
 
 
 
 
 
 
-public void drain()
+
+	public void drain()
 	{
 		// TODO:
 	}
@@ -477,8 +483,8 @@ public void drain()
 */
 
 
+		}
 	}
-}
 
 
 
