e5f2419e9028591ee1f1ce16b916965b76cc5db9
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2001-07-27 10:44:26 +0000

    added detailed output of properties

--- test/AlsaMixerTest.java
+++ test/AlsaMixerTest.java
@@ -3,6 +3,7 @@
  */
 
 import	org.tritonus.lowlevel.alsa.AlsaMixer;
+import	org.tritonus.lowlevel.alsa.AlsaMixerElement;
 
 
 public class AlsaMixerTest
@@ -28,8 +29,50 @@ public class AlsaMixerTest
 			for (int i = 0; i < nReturn; i++)
 			{
 				System.out.println("" + i + " " + anIndices[i] + " " + astrNames[i]);
+				AlsaMixerElement	element = new AlsaMixerElement(mixer, anIndices[i], astrNames[i]);
+				output(element);
 			}
 		}
 		mixer.close();
+	}
+
+
+
+	private static void output(AlsaMixerElement element)
+	{
+		System.out.println("  name: " + element.getName());
+		System.out.println("  index: " + element.getName());
+		System.out.println("  playback mono: " + element.isPlaybackMono());
+		for (int nChannel = AlsaMixerElement.SND_MIXER_SCHN_FRONT_LEFT;
+		     nChannel <= AlsaMixerElement.SND_MIXER_SCHN_WOOFER;
+		     nChannel++)
+		{
+			System.out.println("  playback channel (" + AlsaMixerElement.getChannelName(nChannel) + "): " + element.hasPlaybackChannel(nChannel));
+		}
+		System.out.println("  capture mono: " + element.isCaptureMono());
+		// System.out.println("  capture channel:" + element.hasCaptureChannel());
+		System.out.println("  common volume: " + element.hasCommonVolume());
+		System.out.println("  playback volume: " + element.hasPlaybackVolume());
+		System.out.println("  playback volume joined: " + element.hasPlaybackVolumeJoined());
+		System.out.println("  capture volume: " + element.hasCaptureVolume());
+		System.out.println("  capture volume joined: " + element.hasCaptureVolumeJoined());
+		System.out.println("  common switch: " + element.hasCommonSwitch());
+		System.out.println("  playback switch: " + element.hasPlaybackSwitch());
+		System.out.println("  playback switch joined: " + element.hasPlaybackSwitchJoined());
+		System.out.println("  capture switch: " + element.hasCaptureSwitch());
+		System.out.println("  capture switch joined: " + element.hasCaptureSwitchJoinded());
+		System.out.println("  capture switch exclusive: " + element.hasCaptureSwitchExclusive());
+		if (element.hasCaptureSwitchExclusive())
+		{
+			System.out.println("  capture group: " + element.getCaptureGroup());
+		}
+/*
+		System.out.println("  : " + element.get());
+		System.out.println("  : " + element.get());
+		System.out.println("  : " + element.get());
+		System.out.println("  : " + element.get());
+		System.out.println("  : " + element.get());
+		System.out.println("  : " + element.get());
+*/
 	}
 }
