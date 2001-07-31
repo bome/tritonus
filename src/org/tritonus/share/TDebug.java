8c673974a342bbb61f666b40e728f21873fd2f34
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2001-07-31 22:51:57 +0000

    removed obsolete flags

--- src/org/tritonus/share/TDebug.java
+++ src/org/tritonus/share/TDebug.java
@@ -83,12 +83,9 @@ public class TDebug
 	public static boolean	TraceAlsaSeqDetails = getBooleanProperty("TraceAlsaSeqDetails");
 	public static boolean	TraceAlsaSeqNative = getBooleanProperty("TraceAlsaSeqNative");
 	public static boolean	TracePortScan = getBooleanProperty("TracePortScan");
-	public static boolean	TraceTMidiDevice = getBooleanProperty("TraceTMidiDevice");
-	public static boolean	TraceTSequencer = getBooleanProperty("TraceTSequencer");
 	public static boolean	TraceAlsaMidiDevice = getBooleanProperty("TraceAlsaMidiDevice");
 	public static boolean	TraceAlsaMidiIn = getBooleanProperty("TraceAlsaMidiIn");
 	public static boolean	TraceAlsaMidiOut = getBooleanProperty("TraceAlsaMidiOut");
-	public static boolean	TraceAlsaSequencer = getBooleanProperty("TraceAlsaSequencer");
 	public static boolean	TraceAlsaMidiChannel = getBooleanProperty("TraceAlsaMidiChannel");
 	public static boolean	TraceMshMidiDevice = getBooleanProperty("TraceMshMidiDevice");
 	// TODO: should replace TSequencer and AlsaSequencer
