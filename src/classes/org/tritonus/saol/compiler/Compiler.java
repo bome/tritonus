92d8a1caf918e90afb5778e9f1dcfdd1e68f16b4
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2002-04-21 11:56:57 +0000

    reorganized; now creating instances of this class (instead of static-only execution); adapted package names

--- src/classes/org/tritonus/saol/compiler/Compiler.java
+++ src/classes/org/tritonus/saol/compiler/Compiler.java
@@ -23,63 +23,76 @@
 package	org.tritonus.saol.compiler;
 
 
+import	java.io.File;
 import	java.io.Reader;
 import	java.io.FileReader;
 import	java.io.BufferedReader;
 import	java.io.PushbackReader;
+import	java.io.IOException;
 
-import	saol.parser.*;
-import	saol.lexer.*;
-import	saol.node.*;
+import	java.util.HashMap;
+import	java.util.Iterator;
+import	java.util.Map;
+
+import	org.tritonus.share.TDebug;
+import	org.tritonus.saol.sablecc.parser.*;
+import	org.tritonus.saol.sablecc.lexer.*;
+import	org.tritonus.saol.sablecc.node.*;
 
 
 
 public class Compiler
 {
+	private static final boolean	DEBUG = false;
+
 	private static final int	ACTION_DUMP_TREE = 0;
 	private static final int	ACTION_COMPILE_INSTRUMENTS = 1;
 	private static final int	ACTION_GENERATE_MP4 = 2;
 
 
-	public static void main(String[] arguments)
+	private File		m_saolFile;
+	private int		m_nAction;
+	private Map		m_instrumentMap;
+
+
+	public Compiler(File saolFile)
 	{
-		int	nAction = ACTION_COMPILE_INSTRUMENTS;
-		String	strInputFilename = arguments[0];
-		if (arguments[0].equals("-d"))
-		{
-			nAction = ACTION_DUMP_TREE;
-			strInputFilename = arguments[1];
-		}
-		try
-		{
-			//System.out.println("Type an arithmetic expression:");
+		this(saolFile, ACTION_COMPILE_INSTRUMENTS);
+	}
 
-// Create a Parser instance.
-//			Reader	inputStreamReader = new InputStreamReader(System.in);
-			Reader	reader = new FileReader(strInputFilename);
-			reader = new BufferedReader(reader);
-			PushbackReader	pbReader = new PushbackReader(reader, 1024);
-			Lexer	lexer = new Lexer(pbReader);
-			Parser	parser = new Parser(lexer);
-			Start	tree = parser.parse();
 
-// Apply the translation.
-			switch (nAction)
-			{
-			case ACTION_DUMP_TREE:
-				dumpTree(tree);
-				break;
+	public Compiler(File saolFile, int nAction)
+	{
+		m_saolFile = saolFile;
+		m_nAction = nAction;
+	}
 
-			case ACTION_COMPILE_INSTRUMENTS:
-				compileInstruments(tree);
-				break;
-			}
-		}
-		catch(Exception e)
+
+
+	public void compile()
+		throws Exception
+	{
+		Reader	reader = new FileReader(m_saolFile);
+		reader = new BufferedReader(reader);
+		PushbackReader	pbReader = new PushbackReader(reader, 1024);
+		Lexer	lexer = new Lexer(pbReader);
+		Parser	parser = new Parser(lexer);
+		Start	tree = parser.parse();
+
+		switch (m_nAction)
 		{
-			e.printStackTrace();
+		case ACTION_DUMP_TREE:
+			dumpTree(tree);
+			break;
+
+		case ACTION_COMPILE_INSTRUMENTS:
+			TDebug.out("compiling instruments...");
+			m_instrumentMap = compileInstruments(tree);
+			TDebug.out("IM: " + m_instrumentMap);
+			break;
 		}
 	}
+
 
 
 
@@ -92,20 +105,67 @@ public class Compiler
 	}
 
 
-
-	private static void compileInstruments(Start tree)
+	/**
+	   Returns a Map: instrument names (String) -> instrument classes (Class)
+	 */
+	private static Map compileInstruments(Start tree)
 	{
+		Map		instrumentMap = new HashMap();
 		SAOLGlobals	saolGlobals = new SAOLGlobals();
 		GlobalsSearcher	gsearcher = new GlobalsSearcher(saolGlobals);
 		tree.apply(gsearcher);
-		System.out.println("a-rate: " + saolGlobals.getARate());
-		System.out.println("k-rate: " + saolGlobals.getKRate());
-		System.out.println("inchannels: " + saolGlobals.getInChannels());
-		System.out.println("outchannels: " + saolGlobals.getOutChannels());
-		System.out.println("interp: " + saolGlobals.getInterp());
+		if (DEBUG)
+		{
+			TDebug.out("a-rate: " + saolGlobals.getARate());
+			TDebug.out("k-rate: " + saolGlobals.getKRate());
+			TDebug.out("inchannels: " + saolGlobals.getInChannels());
+			TDebug.out("outchannels: " + saolGlobals.getOutChannels());
+			TDebug.out("interp: " + saolGlobals.getInterp());
+		}
 		// TODO: collection of variable tables, semantic checks
-		InstrumentCompilation	ic = new InstrumentCompilation(saolGlobals);
+		InstrumentCompilation	ic = new InstrumentCompilation(saolGlobals, instrumentMap);
 		tree.apply(ic);
+		Iterator	it = instrumentMap.keySet().iterator();
+		while (it.hasNext())
+		{
+			TDebug.out("" + it.next());
+		}
+		return instrumentMap;
+	}
+
+
+	public Map getInstrumentMap()
+	{
+		if (m_nAction != ACTION_COMPILE_INSTRUMENTS)
+		{
+			TDebug.out("I.M.: returning null");
+			return null;
+		}
+		TDebug.out("I.M.: " + m_instrumentMap);
+		return m_instrumentMap;
+	}
+
+
+
+	public static void main(String[] arguments)
+	{
+		int	nAction = ACTION_COMPILE_INSTRUMENTS;
+		String	strSaolFilename = arguments[0];
+		if (arguments[0].equals("-d"))
+		{
+			nAction = ACTION_DUMP_TREE;
+			strSaolFilename = arguments[1];
+		}
+		File	saolFile = new File(strSaolFilename);
+		Compiler	compiler = new Compiler(saolFile, nAction);
+		try
+		{
+			compiler.compile();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 } 
 
