23488007a8919692642ffdef797cc0076b2aa445
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2002-04-21 19:46:16 +0000

    added comments; added package; now extending Bus which implements the sample accumalation

--- src/classes/org/tritonus/saol/engine/AudioOutputStreamOutput.java
+++ src/classes/org/tritonus/saol/engine/AudioOutputStreamOutput.java
@@ -2,6 +2,27 @@
  *	AudioOutputStreamOutput.java
  */
 
+/*
+ *  Copyright (c) 2002 by Matthias Pfisterer <Matthias.Pfisterer@web.de>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as published
+ *   by the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package	org.tritonus.saol.engine;
+
+
 import	java.io.IOException;
 
 /*
@@ -16,45 +37,39 @@ import  org.tritonus.share.sampled.file.AudioOutputStream;
 
 
 public class AudioOutputStreamOutput
-implements Output
+extends Bus
+implements SystemOutput
 {
 	private AudioOutputStream	m_audioOutputStream;
-	private float			m_fOutput;
+	private byte[]			m_abBuffer;
 
 
 
 	public AudioOutputStreamOutput(AudioOutputStream audioOutputStream)
 	{
+		super(audioOutputStream.getFormat().getChannels());
 		m_audioOutputStream = audioOutputStream;
+		m_abBuffer = new byte[audioOutputStream.getFormat().getFrameSize()];
 	}
 
-
-
-
-	public void init()
-	{
-		m_fOutput = 0.0F;
-	}
-
-
-
-	public void output(float fValue)
-	{
-		// TDebug.out("output(): value: " + fValue);
-		m_fOutput += fValue;
-	}
 
 
 
 	public void emit()
 		throws IOException
 	{
-		float	fOutput = Math.max(Math.min(m_fOutput, 1.0F), -1.0F);
-		// assumes 16 bit linear
-		int	nOutput = (int) (fOutput * 32767.0F);
-		byte[]	abBuffer = new byte[2];
-		TConversionTool.shortToBytes16((short) nOutput, abBuffer, 0, false);
-		m_audioOutputStream.write(abBuffer, 0, abBuffer.length);
+		float[]	afValues = getValues();
+		boolean	bBigEndian = m_audioOutputStream.getFormat().isBigEndian();
+		int	nOffset = 0;
+		for (int i = 0; i < afValues.length; i++)
+		{
+			float	fOutput = Math.max(Math.min(afValues[i], 1.0F), -1.0F);
+			// assumes 16 bit linear
+			int	nOutput = (int) (fOutput * 32767.0F);
+			TConversionTool.shortToBytes16((short) nOutput, m_abBuffer, nOffset, bBigEndian);
+			nOffset += 2;
+		}
+		m_audioOutputStream.write(m_abBuffer, 0, m_abBuffer.length);
 	}
 
 
