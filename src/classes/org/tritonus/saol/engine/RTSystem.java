87ef78e25296fc45cc95d8b48bd4bfb6d0049e05
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2002-04-21 19:52:38 +0000

    added comments; added package; adapted to Output changes; implemented generic instantiation of instrument classes

--- src/classes/org/tritonus/saol/engine/RTSystem.java
+++ src/classes/org/tritonus/saol/engine/RTSystem.java
@@ -2,26 +2,47 @@
  *	RTSystem.java
  */
 
+/*
+ *  Copyright (c) 2002 by Matthias Pfisterer <Matthias.Pfisterer@web.de>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as published
+ *   by the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package	org.tritonus.saol.engine;
+
+
 import	java.io.IOException;
+
+import	java.lang.reflect.Constructor;
 
 import	java.util.LinkedList;
 import	java.util.Iterator;
 import	java.util.List;
+import	java.util.Map;
 
-/*
- *      Tritonus classes.
- *      Using these makes the program not portable to other
- *      Java Sound implementations.
- */
 import  org.tritonus.share.TDebug;
 
 
 
-// IDEA: isolate output into an 'interfaced' class
 public class RTSystem
 extends Thread
 {
-	private Output			m_output;
+	private static final boolean	DEBUG = false;
+
+	private SystemOutput		m_output;
+	private Map			m_instrumentMap;
 	private boolean			m_bRunning;
 	private int			m_nTime;
 	private float			m_fTimeStep;
@@ -36,9 +57,11 @@ extends Thread
 
 
 
-	public RTSystem(Output output)
+	public RTSystem(SystemOutput output, Map instrumentMap)
 	{
 		m_output = output;
+		m_instrumentMap = instrumentMap;
+		// TODO:
 		setRates(44100, 100);
 		m_activeInstruments = new LinkedList();
 		m_scheduledInstruments = new LinkedList();
@@ -96,19 +119,22 @@ extends Thread
 
 	private void doI()
 	{
-		TDebug.out("doI()");
-		TDebug.out("time: " + getTime());
+		if (DEBUG)
+		{
+			TDebug.out("doI()");
+			TDebug.out("time: " + getTime());
+		}
 		synchronized (m_scheduledInstruments)
 		{
 			Iterator	scheduledInstruments = m_scheduledInstruments.iterator();
 			while (scheduledInstruments.hasNext())
 			{
-				TDebug.out("scheduled instrument");
+				if (DEBUG) { TDebug.out("scheduled instrument"); }
 				AbstractInstrument	instrument = (AbstractInstrument) scheduledInstruments.next();
-				TDebug.out("instrument start time: " + instrument.getStartTime());
+				if (DEBUG) { TDebug.out("instrument start time: " + instrument.getStartTime()); }
 				if (getTime() >= instrument.getStartTime())
 				{
-					TDebug.out("...activating");
+					if (DEBUG) { TDebug.out("...activating"); }
 					scheduledInstruments.remove();
 					instrument.doIPass(this);
 					m_activeInstruments.add(instrument);
@@ -121,7 +147,7 @@ extends Thread
 			AbstractInstrument	instrument = (AbstractInstrument) activeInstruments.next();
 			if (getTime() > instrument.getEndTime())
 			{
-					TDebug.out("...DEactivating");
+				if (DEBUG) { TDebug.out("...DEactivating"); }
 				activeInstruments.remove();
 			}
 		}
@@ -149,7 +175,7 @@ extends Thread
 		throws IOException
 	{
 		// TDebug.out("doA()");
-		m_output.init();
+		m_output.clear();
 		Iterator	activeInstruments = m_activeInstruments.iterator();
 		while (activeInstruments.hasNext())
 		{
@@ -171,9 +197,12 @@ extends Thread
 		synchronized (m_scheduledInstruments)
 		{
 			m_scheduledInstruments.add(instrument);
-		TDebug.out("adding instrument");
-		TDebug.out("start: " + nStartTime);
-		TDebug.out("end: " + nEndTime);
+			if (DEBUG)
+			{
+				TDebug.out("adding instrument");
+				TDebug.out("start: " + nStartTime);
+				TDebug.out("end: " + nEndTime);
+			}
 		}
 	}
 
@@ -221,9 +250,18 @@ extends Thread
 
 	private AbstractInstrument createInstrumentInstance(String strInstrumentName)
 	{
-		// TODO:
-		// return new Instrument_tone();
-		return new tone();
+		AbstractInstrument	instrument = null;
+		Class	instrumentClass = (Class) m_instrumentMap.get(strInstrumentName);
+		try
+		{
+			Constructor	constructor = instrumentClass.getConstructor(new Class[]{RTSystem.class});
+			instrument = (AbstractInstrument) constructor.newInstance(new Object[]{this});
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return instrument;
 	}
 }
 
