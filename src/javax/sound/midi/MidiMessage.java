8905438dbf6a357018840247da79d8967a5504d8
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2002-06-15 11:06:19 +0000

    added documentation; cleaned some code according to specification; general cleanup

--- src/javax/sound/midi/MidiMessage.java
+++ src/javax/sound/midi/MidiMessage.java
@@ -3,8 +3,7 @@
  */
 
 /*
- *  Copyright (c) 1999 by Matthias Pfisterer <Matthias.Pfisterer@gmx.de>
- *
+ *  Copyright (c) 1999 - 2002 by Matthias Pfisterer <Matthias.Pfisterer@gmx.de>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as published
@@ -19,118 +18,194 @@
  *   You should have received a copy of the GNU Library General Public
  *   License along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
  */
 
-
-
-
 package	javax.sound.midi;
-
 
 import	org.tritonus.share.midi.MidiUtils;
 
 
+
+/**	Base class for MIDI messages.
+
+	@author Matthias Pfisterer
+*/
 public abstract class MidiMessage
-	implements	Cloneable
+implements Cloneable
 {
-	/**	All data of the message.
+	/**	The data of the message.
+		This array contains the message separated into
+		bytes.
+		The storage format used for this array follows the way messages
+		are sent via a MIDI cable or stored in a standard MIDI file.
+		See the subclasses of MidiMessage to get more information on
+		the required storage format for each type.<p>
+
+		At any time after the execution of this class'
+		constructor,
+		this value must refer to an array with at least one element.
+		Especially, <code>null</code> is not allowed.
+		The array may contain more bytes than used to store the MIDI
+		message. The variable {@link #length length} indicates the number
+		of valid bytes. The content of the additional bytes has to be
+		considered implementation-specific.<p>
+
+		In this implementation, the length of data is always equal to the
+		value of {@link #length length}. However, this is not a
+		specification requirement.
+
+		Accessing this member from subclasses is highly discouraged.
+		Subclasses should use the constructor or
+		{@link #setMessage(byte[], int) setMessage} to modify the content and
+		{@link #getMessage() getMessage()} to retrieve it.
+
+		@see #setMessage(byte[], int)
+		@see #getMessage()
 	 */
 	protected byte[]		data;
 
+
+	/**	The length of the message.
+		This value represents the number of bytes in the array
+		{@link #data data} that are valid.
+		At any time after the execution of this class'
+		constructor,
+		this value is guaranteed to be
+		greater or equal to one.
+		Additinally,  with the exception of while the control flow is inside
+		this class' method {@link #setMessage(byte[], int) setMessage()}
+		the value is guaranteed to be as least as big as the length of
+		{@link #data data}. It may be less than the length of
+		{@link #data data} in cases where {@link #data data}
+		contains additional (invalid) bytes.<p>
+
+		Accessing this member from subclasses is highly discouraged.
+		Subclasses should use the constructor or
+		{@link #setMessage(byte[], int) setMessage()} to modify the length
+		and
+		{@link #getLength() getLength()} to retrieve it.
+
+		@see #getLength()
+	*/
 	protected int			length;
 
 
 
 	/**	Constructs a MidiMessage.
-		The data of the message (data) are initialized by the
-		array passed as a parameter. Note that the data is not copied.
-		Rather, data is set to point at the passed array.
-		This constructor does not use setMessage(byte[], int). So it
+		The passed array is used to initialize {@link #data data}.
+		{@link #length length} is initialized to the length of the passed
+		array. Note that a copy of the passed array is made.<p>
+
+		This constructor does not use
+		{@link #setMessage(byte[], int) setMessage()}. So it is
 		unaffected by overriding setMessage().
+
+		@param abData The data to use to construct the MIDI message.
 	 */
 	protected MidiMessage(byte[] abData)
 	{
-		data = abData;
+		copyToData(abData, abData.length);
 	}
 
 
 
 	/**	Initializes the data of the message.
-		This method 
+		This method sets the data of the MIDI message.
+		<code>nLength</code> bytes of <code>abData</code> are copied to
+		a newly created array, which is set as {@link #data data}.
+		{@link #length length} is initialized to the value of
+		<code>nLength</code>.
+
+		@param abData The bytes to use as MIDI message. This array may
+		contain trailing invalid bytes. <code>nLength</code> gives the
+		number of valid bytes. This means that the length of
+		<code>abData</code> has to be equal or greater than
+		<code>nLength</code>.
+
+		@param nLength The length of the MIDI message. This is equal to
+		the number of valid bytes in <code>abData</code>. This value
+		may be less than the length of <code>abData</code>. In this case,
+		the remaining bytes have to be considered invalid. This means that
+		the value of <code>nLength</code> has to be equal or less than
+		the length of <code>abData</code>.
 	 */
 	protected void setMessage(byte[] abData, int nLength)
-		throws	InvalidMidiDataException
+		throws InvalidMidiDataException
 	{
-		if (nLength < abData.length)
-		{
-			byte[]	abShortenedData = new byte[nLength];
-			System.arraycopy(abData, 0, abShortenedData, 0, nLength);
-			abData = abShortenedData;
-		}
-		data = abData;
-		length = nLength;
+		copyToData(abData, nLength);
 	}
 
 
-	/**	returns the complete message.
-		This method makes a copy of the data and returns a
-		reference to the copy. Both Sun jdk and Tritonus implement it
-		this way. However, it is not clear if this is a requirement
-		(Florian).
-		Behaviour if getLength() != data.length ?? (Matthias, Florian)
+
+	/**
+	   TODO:
+	*/
+	private void copyToData(byte[] abData, int nLength)
+	{
+		synchronized (this)
+		{
+			data = new byte[nLength];
+			System.arraycopy(data, 0, abData, 0, nLength);
+			length = nLength;
+		}
+	}
+
+
+
+	/**	Return the complete message.
+		This method makes a copy of {@link #data data} and returns a
+		reference to the copy.
+		The returned array contains only the bytes that form the MIDI
+		message, even if {@link #data data} contains additional invalid
+		bytes. This requirement also means that the length of the
+		returned array is equal to {@link #length length} and equal
+		to the value returned by {@link #getLength() getLength()}.
+
+		@return An array of bytes representing the MIDI message.
 	 */
 	public byte[] getMessage()
 	{
-		byte[]	abData = new byte[data.length];
-		System.arraycopy(data, 0, abData, 0, data.length);
+		byte[]	abData = new byte[length];
+		System.arraycopy(data, 0, abData, 0, length);
 		return abData;
 	}
 
 
 
 	/**	Returns the status byte of the message.
-	 *	I.e. the first byte is returned. This has to be overridden
-	 *	by subclasses if the status byte is not stored as part of
-	 *	the data (like it is for MetaMessages).
-	 *	If no message has been set, -1 is returned.
-	 */
+		This method returns the first byte of {@link #data data},
+		which is always the status byte.
+
+		@return The status byte of the MIDI message stored in this object.
+	*/
 	public int getStatus()
 	{
-		if (data != null)
-		{
-			return MidiUtils.getUnsignedInteger(data[0]);
-		}
-		else
-		{
-			return -1;
-		}
+		int	nStatus = MidiUtils.getUnsignedInteger(data[0]);
+		return nStatus;
 	}
 
 
 
 	/**	Returns the length of the whole message in bytes.
-		In this implementation, the length returned by this method
+		This returns the value of the member {@link #length length}.
+		The value returned by this method
 		is always equal to the length of the array returned by
-		getMessage(). However, this has to be considered
-		implementation-specific behaviour. It is not guaranteed for
-		other implementations of the Java Sound API. Should be made a
-		requirement? (Matthias, Florian)
-		If no message has been set, a length of -1 is returned.
+		{@link #getMessage() getMessage()}.
+
+		@return The length of the MIDI message in bytes.
+
+		@see #length
+		@see #getMessage()
 	*/
 	public int getLength()
 	{
-		if (data != null)
-		{
-			return data.length;
-		}
-		else
-		{
-			return -1;
-		}
+		return length;
 	}
 
 
+	/**	Create a copy of this MIDI message.
+		@return A new MidiMessage object that is a copy of this one.
+	*/
 	public abstract Object clone();
 }
 
