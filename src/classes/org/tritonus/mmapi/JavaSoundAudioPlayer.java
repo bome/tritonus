38a9143970ce6d944f4dc250c21979cc7f603a5e
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2002-07-04 19:15:40 +0000

    import

---
+++ src/classes/org/tritonus/mmapi/JavaSoundAudioPlayer.java
@@ -0,0 +1,52 @@
+/*
+ *	JavaSoundAudioPlayer.java
+ */
+
+package	org.tritonus.mmapi;
+
+import javax.microedition.media.protocol.DataSource;
+
+
+public class JavaSoundAudioPlayer
+extends TAudioPlayer
+implements Runnable
+{
+	public JavaSoundAudioPlayer(DataSource dataSource)
+	{
+		super(dataSource);
+	}
+
+
+
+	protected void doRealize()
+	{
+	}
+
+
+
+	protected void doPrefetch()
+	{
+	}
+
+
+
+	protected void doStart()
+	{
+	}
+
+
+
+	protected void doStop()
+	{
+	}
+
+
+
+	public void run()
+	{
+	}
+}
+
+
+
+/*** JavaSoundAudioPlayer.java ***/
