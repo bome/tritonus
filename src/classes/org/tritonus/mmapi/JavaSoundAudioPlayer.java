df7a68185d733900a961578cba94f7ab38d14dc8
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2002-07-12 14:04:55 +0000

    implemented transition methods; added inner class SourceInputStream

--- src/classes/org/tritonus/mmapi/JavaSoundAudioPlayer.java
+++ src/classes/org/tritonus/mmapi/JavaSoundAudioPlayer.java
@@ -22,13 +22,51 @@
 
 package	org.tritonus.mmapi;
 
+import java.io.InputStream;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.microedition.media.MediaException;
 import javax.microedition.media.protocol.DataSource;
+import javax.microedition.media.protocol.SourceStream;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
+import org.tritonus.mmapi.URLDataSource.URLSourceStream;
+
+/*
+  TODO (general):
+  - this player
+  - event delivery
+  - event generation
+  - StopTime, VolumeControl
+  - realize()/deallocate()
+  - properties
+  - rework playTone()
+  - test InputStreamDataSurce
+  - more player tests
+ */
+
+/**
+*/
 public class JavaSoundAudioPlayer
 extends TAudioPlayer
 implements Runnable
 {
+	private static final int	BUFFER_SIZE = 128000;
+
+	private AudioInputStream	m_audioInputStream;
+	private SourceDataLine		m_sourceDataLine;
+	private Thread			m_thread;
+
+
+
 	public JavaSoundAudioPlayer(DataSource dataSource)
 	{
 		super(dataSource);
@@ -37,31 +75,142 @@ implements Runnable
 
 
 	protected void doRealize()
+		throws Exception
 	{
+		DataSource	dataSource = getDataSource();
+		dataSource.connect();
+		dataSource.start();
+		SourceStream[]	aSourceStreams = dataSource.getStreams();
+		SourceStream	sourceStream = aSourceStreams[0];
+		if (sourceStream instanceof URLSourceStream)
+		{
+			URL	url = ((URLSourceStream) sourceStream).getURL();
+			m_audioInputStream = AudioSystem.getAudioInputStream(url);
+		}
+		else
+		{
+			InputStream	inputStream = new SourceInputStream(aSourceStreams[0]);
+			m_audioInputStream = AudioSystem.getAudioInputStream(inputStream);
+		}
+		Line.Info	info = new DataLine.Info(SourceDataLine.class, getAudioFormat());
+		m_sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
 	}
 
 
 
 	protected void doPrefetch()
+		throws Exception
 	{
+		getSourceDataLine().open(getAudioFormat());
+		m_thread = new Thread(this);
+		m_thread.start();
 	}
 
 
 
 	protected void doStart()
+		throws Exception
 	{
+		getSourceDataLine().start();
 	}
 
 
 
 	protected void doStop()
+		throws Exception
 	{
+		getSourceDataLine().stop();
+	}
+
+
+
+	protected void doDeallocate()
+		throws Exception
+	{
+		// TODO:
+	}
+
+
+
+	protected void doClose()
+		throws Exception
+	{
+		// TODO:
 	}
 
 
 
 	public void run()
 	{
+		byte[]	abBuffer = new byte[BUFFER_SIZE];
+		while (true)
+		{
+		}
+	}
+
+
+
+	private AudioInputStream getAudioInputStream()
+	{
+		return m_audioInputStream;
+	}
+
+
+
+	private AudioFormat getAudioFormat()
+	{
+		return m_audioInputStream.getFormat();
+	}
+
+
+
+	private SourceDataLine getSourceDataLine()
+	{
+		return m_sourceDataLine;
+	}
+
+
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+
+
+	private class SourceInputStream
+	extends InputStream
+	{
+		private SourceStream	m_sourceStream;
+		private byte[]		m_abOneByteBuffer;
+
+
+		public SourceInputStream(SourceStream sourceStream)
+		{
+			m_sourceStream = sourceStream;
+		}
+
+
+
+		public int read()
+			throws IOException
+		{
+			if (m_abOneByteBuffer == null)
+			{
+				m_abOneByteBuffer = new byte[1];
+			}
+			read(m_abOneByteBuffer);
+			return m_abOneByteBuffer[0];
+		}
+
+
+
+		public int read(byte[] abData,
+				int nOffset,
+				int nLength)
+			throws IOException
+		{
+			int	nReturn = m_sourceStream.read(abData,
+							      nOffset,
+							      nLength);
+			return nReturn;
+		}
 	}
 }
 
