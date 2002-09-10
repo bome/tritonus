6759a32378bf0a25f239aaf341bdc0c1b1665929
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2002-09-10 10:16:59 +0000

    added some documentation

--- src/javax/sound/sampled/SourceDataLine.java
+++ src/javax/sound/sampled/SourceDataLine.java
@@ -5,7 +5,6 @@
 /*
  *  Copyright (c) 1999 by Matthias Pfisterer <Matthias.Pfisterer@gmx.de>
  *
- *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as published
  *   by the Free Software Foundation; either version 2 of the License, or
@@ -19,9 +18,7 @@
  *   You should have received a copy of the GNU Library General Public
  *   License along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
  */
-
 
 package	javax.sound.sampled;
 
@@ -29,7 +26,7 @@ package	javax.sound.sampled;
 
 
 public interface SourceDataLine
-	extends		DataLine
+extends DataLine
 {
 	public void open(AudioFormat audioFormat,
 			 int nBufferSize)
@@ -40,6 +37,18 @@ public interface SourceDataLine
 		throws	LineUnavailableException;
 
 
+	/** Write data to the line.
+
+	@param abData The buffer to use.
+
+	@param nOffset
+
+	@param nLength The length of the data that should be written,
+	in bytes. Can be less that the length of abData.
+
+	@return The number of bytes written. May be less than nLength.
+
+	*/
 	public int write(byte[] abData,
 			 int nOffset,
 			 int nLength);
