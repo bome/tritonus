2d48a75e933e3ad7b8e3c608eecdb2e79bf804a2
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2004-01-05 11:21:36 +0000

    added test cases for properties

---
+++ test/suite/src/classes/org/tritonus/test/TAudioFileFormatTestCase.java
@@ -0,0 +1,117 @@
+/*
+ *	TAudioFileFormatTestCase.java
+ */
+
+/*
+ *  Copyright (c) 2003 by Matthias Pfisterer
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as published
+ *   by the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.tritonus.test;
+
+import junit.framework.TestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.tritonus.share.sampled.file.TAudioFileFormat;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+
+
+
+public class TAudioFileFormatTestCase
+extends TestCase
+{
+	public TAudioFileFormatTestCase(String strName)
+	{
+		super(strName);
+	}
+
+
+
+	public void testEmptyMap()
+	{
+		Map prop = new HashMap();
+		TAudioFileFormat fileFormat = new TAudioFileFormat(
+			null, null,
+			AudioSystem.NOT_SPECIFIED,
+			AudioSystem.NOT_SPECIFIED,
+			prop);
+		Map propReturn = fileFormat.properties();
+		assertTrue(propReturn.isEmpty());
+		Object result = propReturn.get("bitrate");
+		assertNull(result);
+	}
+
+
+
+	public void testCopying()
+	{
+		Map prop = new HashMap();
+		prop.put("bitrate", new Float(22.5F));
+		TAudioFileFormat fileFormat = new TAudioFileFormat(
+			null, null,
+			AudioSystem.NOT_SPECIFIED,
+			AudioSystem.NOT_SPECIFIED,
+			prop);
+		Map propReturn = fileFormat.properties();
+		assertTrue(prop != propReturn);
+		prop.put("bitrate", new Float(42.5F));
+		Object result = propReturn.get("bitrate");
+		assertEquals(new Float(22.5F), result);
+	}
+
+
+	public void testUnmodifiable()
+	{
+		Map prop = new HashMap();
+		TAudioFileFormat fileFormat = new TAudioFileFormat(
+			null, null,
+			AudioSystem.NOT_SPECIFIED,
+			AudioSystem.NOT_SPECIFIED,
+			prop);
+		Map propReturn = fileFormat.properties();
+		try
+		{
+			propReturn.put("author", "Matthias Pfisterer");
+			fail("returned Map allows modifications");
+		}
+		catch (UnsupportedOperationException e)
+		{
+		}
+	}
+
+
+	public void testGet()
+	{
+		Map prop = new HashMap();
+		prop.put("bitrate", new Float(22.5F));
+		prop.put("author", "Matthias Pfisterer");
+		TAudioFileFormat fileFormat = new TAudioFileFormat(
+			null, null,
+			AudioSystem.NOT_SPECIFIED,
+			AudioSystem.NOT_SPECIFIED,
+			prop);
+		Map propReturn = fileFormat.properties();
+		assertEquals(new Float(22.5F), propReturn.get("bitrate"));
+		assertEquals("Matthias Pfisterer", propReturn.get("author"));
+	}
+}
+
+
+
+/*** TAudioFileFormatTestCase.java ***/
