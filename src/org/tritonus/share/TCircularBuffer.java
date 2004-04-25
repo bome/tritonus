a39fd9b55b976642b027209ff061531388d97171
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2004-04-25 08:33:01 +0000

    changed comment

--- src/org/tritonus/share/TCircularBuffer.java
+++ src/org/tritonus/share/TCircularBuffer.java
@@ -3,7 +3,7 @@
  */
 
 /*
- *  Copyright (c) 1999 by Matthias Pfisterer <Matthias.Pfisterer@gmx.de>
+ *  Copyright (c) 1999 by Matthias Pfisterer
  *
  *
  *   This program is free software; you can redistribute it and/or modify
--- src/org/tritonus/share/sampled/AudioSystemShadow.java
+++ src/org/tritonus/share/sampled/AudioSystemShadow.java
@@ -5,7 +5,7 @@
  */
 
 /*
- *  Copyright (c) 1999, 2000 by Matthias Pfisterer <Matthias.Pfisterer@gmx.de>
+ *  Copyright (c) 1999, 2000 by Matthias Pfisterer
  *
  *
  *   This program is free software; you can redistribute it and/or modify
