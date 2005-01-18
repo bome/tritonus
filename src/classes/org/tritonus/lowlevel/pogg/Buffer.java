eb34720081ada8029ca590af70566740e15866a8
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2005-01-18 18:54:11 +0000

    added convenience method

--- src/classes/org/tritonus/lowlevel/pogg/Buffer.java
+++ src/classes/org/tritonus/lowlevel/pogg/Buffer.java
@@ -5,7 +5,7 @@
  */
 
 /*
- *  Copyright (c) 2000 - 2004 by Matthias Pfisterer
+ *  Copyright (c) 2000 - 2005 by Matthias Pfisterer
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as published
@@ -257,6 +257,13 @@ public class Buffer
 		return s;
 	}
 
+
+	/** Reads a single bit.
+	 */
+	public boolean readFlag()
+	{
+		return (read(1) != 0);
+	}
 
 	private static native void setTrace(boolean bTrace);
 
