cb6ce0a6e7cfdbf9de0dfd96b8dc618c02a39aec
Florian Bomers <github@bome.com>
2007-06-03 19:53:46 +0000

    support for FloatSampleInput

--- src/classes/org/tritonus/share/sampled/convert/TSynchronousFilteredAudioInputStream.java
+++ src/classes/org/tritonus/share/sampled/convert/TSynchronousFilteredAudioInputStream.java
@@ -37,6 +37,8 @@ import javax.sound.sampled.AudioInputStream;
 
 import org.tritonus.share.TDebug;
 import org.tritonus.share.sampled.AudioUtils;
+import org.tritonus.share.sampled.FloatSampleBuffer;
+import org.tritonus.share.sampled.FloatSampleInput;
 
 
 
@@ -48,14 +50,20 @@ import org.tritonus.share.sampled.AudioUtils;
  * @author Florian Bomers
  */
 public abstract class TSynchronousFilteredAudioInputStream
-extends TAudioInputStream {
+extends TAudioInputStream implements FloatSampleInput {
 
 	private AudioInputStream originalStream;
+	
+	/** the same originalStream cast to FloatSampleInput, if it is one */
+	private FloatSampleInput originalStreamFloat;
+	
 	private AudioFormat originalFormat;
 	/** 1 if original format's frame size is NOT_SPECIFIED */
 	private int originalFrameSize;
 	/** 1 if original format's frame size is NOT_SPECIFIED */
 	private int newFrameSize;
+	
+	private boolean EOF = false;
 
 	/**
 	 * The intermediate buffer used during convert actions
@@ -71,6 +79,9 @@ extends TAudioInputStream {
 	 * it will be set to true when (frameSizeFactor==1)
 	 */
 	private boolean	m_bConvertInPlace = false;
+	
+	/** if this flag is set, convert(FloatSampleBuffer) is implemented by overriding classes */
+	private boolean m_enableFloatConversion = false;
 
 	public TSynchronousFilteredAudioInputStream(AudioInputStream audioInputStream, AudioFormat newFormat) {
 		// the super class will do nothing... we override everything
@@ -81,25 +92,40 @@ extends TAudioInputStream {
 		                  1 : originalFormat.getFrameSize();
 		newFrameSize=(getFormat().getFrameSize()<=0) ?
 		             1 : getFormat().getFrameSize();
+		if (originalStream instanceof FloatSampleInput) {
+			originalStreamFloat = (FloatSampleInput) originalStream;
+		}
 		if (TDebug.TraceAudioConverter) {
 			TDebug.out("TSynchronousFilteredAudioInputStream: original format ="
 			           +AudioUtils.format2ShortStr(originalFormat));
 			TDebug.out("TSynchronousFilteredAudioInputStream: converted format="
 			           +AudioUtils.format2ShortStr(getFormat()));
 		}
-		//$$fb 2000-07-17: convert in place has to be enabled explicitly with "enableConvertInPlace"
-		//if (getFormat().getFrameSize() == originalFormat.getFrameSize()) {
-		//  m_bConvertInPlace = true;
-		//}
 		m_bConvertInPlace = false;
+		m_enableFloatConversion = false;
 	}
 
+	/**
+	 * descendant classes should call this method if they have implemented
+	 * convertInPlace(). ConvertInPlace will only be used if the converted frame
+	 * size is larger than the original frame size.
+	 */
 	protected boolean enableConvertInPlace() {
 		if (newFrameSize >= originalFrameSize) {
 			m_bConvertInPlace = true;
 		}
 		return m_bConvertInPlace;
 	}
+	
+	/**
+	 * Descendant classes should call this method if they have implemented
+	 * convert(FloatSampleBuffer). That convert method will only be called 
+	 * if this class' FloatSampleInput.read() is used.
+	 */
+	protected void enableFloatConversion() {
+		m_enableFloatConversion = true;
+	}
+	
 
 
 	/**
@@ -122,8 +148,23 @@ extends TAudioInputStream {
 	 * This method must always convert frameCount frames, so no return value is necessary.
 	 */
 	protected void convertInPlace(byte[] buffer, int byteOffset, int frameCount) {
-		throw new RuntimeException("Illegal call to convertInPlace");
+		throw new RuntimeException("illegal call to convertInPlace");
 	}
+
+	/**
+	 * Override this method to do the actual conversion in the
+	 * FloatSampleBuffer. Use buffer's methods to shrink the number of samples,
+	 * if necessary. This method will only be called if this stream is accessed
+	 * by way of FloatSampleInput methods.
+	 * 
+	 * @param buffer the buffer to convert
+	 * @param offset the offset in buffer in samples
+	 * @param count the number of samples in buffer to convert
+	 */
+	protected void convert(FloatSampleBuffer buffer, int offset, int count) {
+		throw new RuntimeException("illegal call to convert(FloatSampleBuffer)");
+	}
+
 
 	public int read()
 	throws IOException {
@@ -131,7 +172,6 @@ extends TAudioInputStream {
 			throw new IOException("frame size must be 1 to read a single byte");
 		}
 		// very ugly, but efficient. Who uses this method anyway ?
-		// TODO: use an instance variable
 		byte[] temp = new byte[1];
 		int result = read(temp);
 		if (result == -1) {
@@ -145,9 +185,10 @@ extends TAudioInputStream {
 	}
 
 
-
+	/** remove the temporary read buffer to save heap */
 	private void clearBuffer() {
 		m_buffer = null;
+		m_floatByteBuffer = null;
 	}
 
 	public AudioInputStream getOriginalStream() {
@@ -164,7 +205,7 @@ extends TAudioInputStream {
 	 * When nLength is not an integral number of frames,
 	 * this method may read less than nLength bytes.
 	 */
-	public int read(byte[] abData, int nOffset, int nLength)
+	public final int read(byte[] abData, int nOffset, int nLength)
 	throws IOException {
 		// number of frames that we have to read from the underlying stream.
 		int	nFrameLength = nLength/newFrameSize;
@@ -196,6 +237,7 @@ extends TAudioInputStream {
 		if (nBytesRead == -1) {
 			// end of stream
 			clearBuffer();
+			EOF = true;
 			return -1;
 		}
 		int nFramesRead = nBytesRead / originalFrameSize;
@@ -235,6 +277,7 @@ extends TAudioInputStream {
 
 	public void close()
 	throws IOException {
+		EOF = true;
 		originalStream.close();
 		clearBuffer();
 	}
@@ -258,6 +301,114 @@ extends TAudioInputStream {
 		return originalStream.markSupported();
 	}
 
+	// interface FloatSampleInput
+	
+	public int getChannels() {
+		return format.getChannels();
+	}
+
+	public float getSampleRate() {
+		return format.getSampleRate();
+	}
+
+	public boolean isDone() {
+		// if this class was closed, never return open again
+		if (EOF) return true;
+		if (originalStreamFloat != null) {
+			return originalStreamFloat.isDone();
+		}
+		return false;
+	}
+
+	/** temporary byte buffer for conversion from/to byte/float arrays */
+	private byte[] m_floatByteBuffer = null;
+	
+	/**
+	 * read sampleCount converted samples at the specified offset. The current
+	 * implementation requires that offset is 0 and sampleCount ==
+	 * buffer.getSampleCount().
+	 */
+	public void read(FloatSampleBuffer buffer, int offset, int sampleCount) {
+		try {
+			// Case 1: reading cannot, but processing can be done in float
+			// layer,
+			// so read unconverted bytes, then convert to float and process
+			if (originalStreamFloat == null && m_enableFloatConversion) {
+				// currently cannot convert in the middle of the buffer
+				if (offset > 0 || sampleCount != buffer.getSampleCount()) {
+					throw new IllegalArgumentException(
+							"float reading with offset not supported");
+				}
+				// allocate a byte array large enough to hold the byte data
+				int reqSize = sampleCount * originalFrameSize;
+				if (m_floatByteBuffer == null
+						|| m_floatByteBuffer.length < reqSize) {
+					m_floatByteBuffer = new byte[reqSize];
+				}
+				// read into byte array -- is already processed
+				int bytesRead = originalStream.read(m_floatByteBuffer, 0,
+						reqSize);
+				// convert the byte array to float
+				if (bytesRead <= 0) {
+					// EOF or nothing read
+					buffer.setSampleCount(0, false);
+					return;
+				}
+				// convert to float
+				buffer.initFromByteArray(m_floatByteBuffer, 0, bytesRead,
+						originalFormat);
+				// do the processing
+				convert(buffer, 0, buffer.getSampleCount());
+			} else
+			// Case 2: reading or processing cannot be done in float layer,
+			// do the conversion with byte array and convert afterwards
+			if (originalStreamFloat == null || !m_enableFloatConversion) {
+				// currently cannot convert in the middle of the buffer
+				if (offset > 0 || sampleCount != buffer.getSampleCount()) {
+					throw new IllegalArgumentException(
+							"float reading with offset not supported");
+				}
+				// allocate a byte array large enough to hold the converted data
+				int reqSize = sampleCount * format.getFrameSize();
+				if (m_floatByteBuffer == null
+						|| m_floatByteBuffer.length < reqSize) {
+					m_floatByteBuffer = new byte[reqSize];
+				}
+				// read into byte array -- is already processed
+				int bytesRead = read(m_floatByteBuffer, 0, reqSize);
+				// convert the byte array to float
+				if (bytesRead <= 0) {
+					// EOF or nothing read
+					buffer.setSampleCount(0, false);
+					return;
+				}
+				// convert to float
+				buffer.initFromByteArray(m_floatByteBuffer, 0, bytesRead,
+						format);
+			} else {
+				// read from the source stream
+				originalStreamFloat.read(buffer, offset, sampleCount);
+				if (offset + sampleCount > buffer.getSampleCount()) {
+					sampleCount = buffer.getSampleCount() - offset;
+					if (sampleCount < 0) {
+						sampleCount = 0;
+					}
+				}
+				// do the actual processing
+				convert(buffer, offset, sampleCount);
+			}
+
+		} catch (IOException ioe) {
+			if (TDebug.TraceAllExceptions) {
+				ioe.printStackTrace();
+			}
+			buffer.setSampleCount(0, false);
+		}
+	}
+
+	public void read(FloatSampleBuffer buffer) {
+		read(buffer, 0, buffer.getSampleCount());
+	}
 
 }
 
