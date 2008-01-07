81b7b06ec4c6e1d173535d98bf8faf5f4ad590cb
Florian Bomers <github@bome.com>
2008-01-07 16:02:27 +0000

    added frame aligned versions of millis2Bytes(), added millis2Frames(), bytes2Millis(), frames2Millis(), Java Sound Audio Dngine detection, linear2decibel(), decibel2linear()

--- src/classes/org/tritonus/share/sampled/AudioUtils.java
+++ src/classes/org/tritonus/share/sampled/AudioUtils.java
@@ -34,10 +34,12 @@ import java.util.Iterator;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.Mixer;
 
 
 
+@SuppressWarnings("cast")
 public class AudioUtils
 {
 	public static long getLengthInBytes(AudioInputStream audioInputStream)
@@ -82,6 +84,7 @@ public class AudioUtils
 
 
 
+	@SuppressWarnings("unchecked")
 	public static boolean containsFormat(AudioFormat sourceFormat,
 					     Iterator possibleFormats)
 	{
@@ -104,60 +107,120 @@ public class AudioUtils
 		return ((sampleSizeInBits + 7) / 8) * channels;
 	}
 
-	/**
-	* Conversion milliseconds -> bytes
-	*/
-
+	/** Conversion milliseconds to bytes */
 	public static long millis2Bytes(long ms, AudioFormat format) {
-		return millis2Bytes(ms, format.getFrameRate(), format.getFrameSize());
+		return millis2Bytes(ms, (double) format.getFrameRate(), format.getFrameSize());
 	}
-
+	/** Conversion milliseconds to bytes */
 	public static long millis2Bytes(long ms, double frameRate, int frameSize) {
 		return (long) (ms*frameRate/1000*frameSize);
 	}
-
+	/** convert milliseconds to bytes. Be careful to not exceed the integer maximum value */
+	public static int millis2Bytes(int ms, AudioFormat format) {
+		return millis2Bytes(ms, (double) format.getFrameRate(), format.getFrameSize());
+	}
+	/** convert milliseconds to bytes. Be careful to not exceed the integer maximum value */
+	public static int millis2Bytes(int ms, double frameRate, int frameSize) {
+		return (int) (ms*frameRate/1000*frameSize);
+	}
+	/** convert milliseconds to bytes. */
 	public static long millis2Bytes(double ms, AudioFormat format) {
 		return millis2Bytes(ms, format.getFrameRate(), format.getFrameSize());
 	}
-
+	/** convert milliseconds to bytes. */
 	public static long millis2Bytes(double ms, double frameRate, int frameSize) {
-		return ((long) (ms*frameRate/1000.0))*frameSize;
+		return (long) (ms*frameRate/1000.0*frameSize);
 	}
 
-	/**
-	* Conversion milliseconds -> bytes (bytes will be frame-aligned)
-	*/
+	/** Conversion milliseconds to bytes, return value is frame-aligned */
 	public static long millis2BytesFrameAligned(long ms, AudioFormat format) {
-		return millis2BytesFrameAligned(ms, format.getFrameRate(), format.getFrameSize());
+		return millis2BytesFrameAligned(ms, (double) format.getFrameRate(), format.getFrameSize());
 	}
-
-	public static long millis2BytesFrameAligned(long ms, float frameRate, int frameSize) {
+	/** Conversion milliseconds to bytes, return value is frame-aligned */
+	public static long millis2BytesFrameAligned(long ms, double frameRate, int frameSize) {
+		return ((long) (ms*frameRate/1000))*frameSize;
+	}
+	/** Conversion milliseconds to bytes, return value is frame-aligned */
+	public static int millis2BytesFrameAligned(int ms, AudioFormat format) {
+		return millis2BytesFrameAligned(ms, (double) format.getFrameRate(), format.getFrameSize());
+	}
+	/** Conversion milliseconds to bytes, return value is frame-aligned */
+	public static int millis2BytesFrameAligned(int ms, double frameRate, int frameSize) {
+		return ((int) (ms*frameRate/1000))*frameSize;
+	}
+	/** Conversion milliseconds to bytes, return value is frame-aligned */
+	public static long millis2BytesFrameAligned(double ms, AudioFormat format) {
+		return millis2BytesFrameAligned(ms, (double) format.getFrameRate(), format.getFrameSize());
+	}
+	/** Conversion milliseconds to bytes, return value is frame-aligned */
+	public static long millis2BytesFrameAligned(double ms, double frameRate, int frameSize) {
 		return ((long) (ms*frameRate/1000))*frameSize;
 	}
 
-	/**
-	* Conversion milliseconds -> frames
-	*/
+	/** Conversion milliseconds to frames (samples) */
 	public static long millis2Frames(long ms, AudioFormat format) {
-		return millis2Frames(ms, format.getFrameRate());
+		return millis2Frames(ms, (double) format.getFrameRate());
+	}
+	/** Conversion milliseconds to frames (samples) */
+	public static long millis2Frames(long ms, double frameRate) {
+		return (long) (ms*frameRate/1000.0);
+	}
+	/** Conversion milliseconds to frames (samples) */
+	public static int millis2Frames(int ms, AudioFormat format) {
+		return millis2Frames(ms, (double) format.getFrameRate());
+	}
+	/** Conversion milliseconds to frames (samples) */
+	public static int millis2Frames(int ms, double frameRate) {
+		return (int) (ms*frameRate/1000.0);
+	}
+	/** Conversion milliseconds to frames (samples) */
+	public static long millis2Frames(double ms, AudioFormat format) {
+		return (long) millis2FramesD(ms, (double) format.getFrameRate());
+	}
+	/** Conversion milliseconds to frames (samples) */
+	public static long millis2Frames(double ms, double frameRate) {
+		return (long) millis2FramesD(ms, frameRate);
+	}
+	/** Conversion milliseconds to frames (samples) */
+	public static double millis2FramesD(double ms, AudioFormat format) {
+		return millis2FramesD(ms, (double) format.getFrameRate());
+	}
+	/** Conversion milliseconds to frames (samples) */
+	public static double millis2FramesD(double ms, double frameRate) {
+		return ms*frameRate/1000.0;
 	}
 
-	public static long millis2Frames(long ms, float frameRate) {
-		return (long) (ms*frameRate/1000);
-	}
-
-	/**
-	* Conversion bytes -> milliseconds 
-	*/
+	/** Conversion bytes to milliseconds */
 	public static long bytes2Millis(long bytes, AudioFormat format) {
-		return (long) (bytes/format.getFrameRate()*1000/format.getFrameSize());
+		return (long) frames2MillisD(bytes/format.getFrameSize(), format.getFrameRate());
 	}
-
-	/**
-	* Conversion frames -> milliseconds 
-	*/
+	/** Conversion bytes to milliseconds */
+	public static int bytes2Millis(int bytes, AudioFormat format) {
+		return (int) frames2MillisD(bytes/format.getFrameSize(), format.getFrameRate());
+	}
+	/** Conversion bytes to milliseconds */
+	public static double bytes2MillisD(long bytes, AudioFormat format) {
+		return frames2MillisD(bytes/format.getFrameSize(), format.getFrameRate());
+	}
+	/** Conversion bytes to milliseconds */
+	public static double bytes2MillisD(long bytes, double frameRate, int frameSize) {
+		return frames2MillisD(bytes/frameSize, frameRate);
+	}
+	/** Conversion frames to milliseconds */
 	public static long frames2Millis(long frames, AudioFormat format) {
-		return (long) (frames/format.getFrameRate()*1000);
+		return (long) frames2MillisD(frames, format.getFrameRate());
+	}
+	/** Conversion frames to milliseconds */
+	public static int frames2Millis(int frames, AudioFormat format) {
+		return (int) frames2MillisD(frames, format.getFrameRate());
+	}
+	/** Conversion frames to milliseconds */
+	public static double frames2MillisD(long frames, AudioFormat format) {
+		return frames2MillisD(frames, format.getFrameRate());
+	}
+	/** Conversion frames to milliseconds */
+	public static double frames2MillisD(long frames, double frameRate) {
+		return frames/frameRate*1000.0;
 	}
 	
 	/**
@@ -181,16 +244,34 @@ public class AudioUtils
 	
 	/**
 	 * Return if the passed mixer info is the Java Sound Audio Engine.
+	 * 
 	 * @param mixerInfo the mixer info to query
 	 * @return true if the mixer info describes the Java Sound Audio Engine
 	 */
 	public static boolean isJavaSoundAudioEngine(Mixer.Info mixerInfo) {
-		return mixerInfo.getName().equals("Java Sound Audio Engine");
+		return (mixerInfo != null) && (mixerInfo.getName() != null)
+				&& mixerInfo.getName().equals("Java Sound Audio Engine");
 	}
 	
 	/**
+	 * Return if the passed line is writing to or reading from the Java Sound Audio Engine.
+	 * 
+	 * @param line the data line to query
+	 * @return true if the line is using the Java Sound Audio Engine
+	 */
+	public static boolean isJavaSoundAudioEngine(DataLine line) {
+		if (line != null) {
+			String clazz = line.getClass().toString(); 
+			return clazz.indexOf("MixerSourceLine") >= 0
+			|| clazz.indexOf("MixerClip") >= 0
+			|| clazz.indexOf("SimpleInputDevice") >= 0;
+		}
+		return false;
+	}
+
+		/**
 	 * tries to guess if this program is running on a big endian platform
-	 * @return
+	 * @return true if the system's native endianness is big endian
 	 */
 	public static boolean isSystemBigEndian() {
 		return java.nio.ByteOrder.nativeOrder().equals(java.nio.ByteOrder.BIG_ENDIAN);
@@ -214,7 +295,46 @@ public class AudioUtils
 	    	NS_or_number(format.getSampleSizeInBits()) + "bit-" +
 	    	NS_or_number(((int)format.getSampleRate())) + "Hz-"+
 	    	(format.isBigEndian() ? "be" : "le");
-    } 
+    }
+    
+	/**
+	 * The value used for negative infinity in decibels. The default value is
+	 * -100.0, which is approximately the s/n ratio achieved with 16-bit samples.
+	 * If you use higher resolution, set this to a lower value, like -150.0.
+	 */
+	public static double SILENCE_DECIBEL = -100.0;
+
+	/**
+	 * Get decibel from a linear factor.
+	 * 
+	 * @param linearFactor 0..1..inf
+	 * @return the converted decibel (SILENCE_DECIBEL...0...inf)
+	 */
+	public final static double linear2decibel(double linearFactor) {
+		if (linearFactor <= 0.0) {
+			return SILENCE_DECIBEL;
+		}
+		double ret = Math.log10(linearFactor) * 20.0;
+		if (ret < SILENCE_DECIBEL) {
+			ret = SILENCE_DECIBEL;
+		}
+		return ret;
+	}
+
+	/**
+	 * Calculate the linear factor corresponding to the specified decibel level.
+	 * 
+	 * @param decibels [SILENCE_DECIBEL...0...inf]
+	 * @return linear factor [0...1...inf]
+	 */
+	public final static double decibel2linear(double decibels) {
+		if (decibels <= SILENCE_DECIBEL) {
+			return 0.0;
+		}
+		return Math.pow(10.0, decibels * 0.05);
+	}
+
+    
 
 }
 
