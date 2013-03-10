5b81f64908e64bca5ac7f734a5cf5c77f7cefd21
Matthias Pfisterer <pfisterer@users.sourceforge.net>
2013-03-10 00:47:48 +0000

    refactoring of GSM decoder

--- src/classes/org/tritonus/lowlevel/gsm/BitDecoder.java
+++ src/classes/org/tritonus/lowlevel/gsm/BitDecoder.java
@@ -8,14 +8,9 @@ public class BitDecoder
     }
 
     private AllocationMode allocationMode;
-    /**
-     * The following variables are for exploding Microsoft frames. See
-     * {@link #explodeFrameMicrosoft(byte[], int, boolean)}.
-     */
 
-    // TODO: (GSM) should be reverted to private once transition is finished
-    public byte[] m_codedFrame;
-    public int m_codedFrameByteIndex;
+    private byte[] m_codedFrame;
+    private int m_codedFrameByteIndex;
     private int m_sr;
     private int m_currentBits;
 
@@ -36,7 +31,13 @@ public class BitDecoder
         m_currentBits = 0;
     }
 
-    public final void getNextCodedByteValue()
+    public void setCodedFrame(byte[] c, final int bufferStartIndex)
+    {
+        m_codedFrame = c;
+        m_codedFrameByteIndex = bufferStartIndex;
+    }
+
+    private final void getNextCodedByteValue()
     {
         m_sr |= getNextCodedByteValue2() << m_currentBits;
         m_currentBits += 8;
@@ -51,55 +52,20 @@ public class BitDecoder
 
     public final int getNextBits(int bits)
     {
-        int value = m_sr & Gsm_Def.BITMASKS[bits];
-        m_sr >>>= bits;
-        m_currentBits -= bits;
-        return value;
-    }
-
-    public final void addBits(int value, int numBits)
-    {
-        // while (numBits > 0)
-        // {
-        // if (byteIndex >= codedBytes.length)
-        // {
-        // throw new RuntimeException("No more bytes in coded bytes array");
-        // }
-        // int bits = Math.min(numBits, remainingBitsInCurrentByte);
-        // int nextRemainingBits = remainingBitsInCurrentByte - bits;
-        // int nextNumBits = numBits - bits;
-        // int x;
-        // switch (allocationMode)
-        // {
-        // case LSBitFirst:
-        // x = (((value) & Gsm_Def.BITMASKS[bits]) << (8 -
-        // remainingBitsInCurrentByte));
-        // codedBytes[byteIndex] |= x;
-        // value >>>= bits;
-        // break;
-        // case MSBitFirst:
-        // x = (((value >>> nextNumBits) & Gsm_Def.BITMASKS[bits]) <<
-        // nextRemainingBits);
-        // codedBytes[byteIndex] |= x;
-        // break;
-        // }
-        // remainingBitsInCurrentByte = nextRemainingBits;
-        // if (remainingBitsInCurrentByte == 0)
-        // {
-        // byteIndex++;
-        // remainingBitsInCurrentByte = 8;
-        // }
-        // numBits = nextNumBits;
-        // }
-    }
-
-    public int getSr()
-    {
-        return m_sr;
-    }
-
-    public void setSr(int sr)
-    {
-        m_sr = sr;
+        switch (allocationMode)
+        {
+        case LSBitFirst:
+            while (m_currentBits < bits)
+            {
+                getNextCodedByteValue();
+            }
+            int value = m_sr & Gsm_Def.BITMASKS[bits];
+            m_sr >>>= bits;
+            m_currentBits -= bits;
+            return value;
+        case MSBitFirst:
+        default:
+            throw new RuntimeException("not supported");
+        }
     }
 }
