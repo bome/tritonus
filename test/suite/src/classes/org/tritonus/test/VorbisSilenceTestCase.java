66009f43c228d4fa8e82d22eee873157708b762c
Florian Bomers <florian.bomers@bome.com>
2018-09-12 17:35:01 +0200

    License Update to Apache 2.0

--- test/suite/src/classes/org/tritonus/test/VorbisSilenceTestCase.java
+++ test/suite/src/classes/org/tritonus/test/VorbisSilenceTestCase.java
@@ -5,6 +5,8 @@
 /*
  *  Copyright (c) 2003 by Dan Rollo
  *
+ *  [license not updated to Apache 2.0]
+ *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as published
  *   by the Free Software Foundation; either version 2 of the License, or
